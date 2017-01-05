eed8a79f94f800cb282f6fcf66afaf2b832076de
James Crooks <crooks1379@gmail.com>
2017-01-05 17:28:47 -0600

    Handlers, Middleware, and Chain - 1

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,33 @@ extern crate iron;
 extern crate router;
 
 use iron::prelude::*;
+use iron::Handler;
 use iron::status;
 
 use router::Router;
 
 fn main() {
     let mut router = Router::new();
+    let root_handler = StringResponseHandler::new(String::from("Test\n"));
     router.get("/", root_handler, "index");
     Iron::new(router).http("localhost:9001").unwrap();
 }
 
-fn root_handler(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Hello Iron!\n")))
+struct StringResponseHandler {
+    response_message: String
+}
+
+impl StringResponseHandler {
+    fn new(message: String) -> StringResponseHandler {
+        StringResponseHandler {
+            response_message: message,
+        }
+    }
+}
+
+impl Handler for StringResponseHandler {
+    fn handle(&self, _: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, self.response_message.clone())))
+    }
 }
 
